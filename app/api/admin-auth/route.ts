7e325bbc1d33c6bcf03f8c5f912a43ac223b10e7
Nathaniel James <nathanjamestonacao@gmail.com>
2026-04-12 15:27:07 +0000

    feat: password protected admin page

--- app/api/admin-auth/route.ts
+++ app/api/admin-auth/route.ts
@@ -4,6 +4,9 @@ export async function POST(req: NextRequest) {
   const { password } = await req.json()
   const correct = process.env.ADMIN_PASSWORD
 
+  console.log('ENV PASSWORD:', correct)
+  console.log('ENTERED:', password)
+
   if (!correct) {
     return NextResponse.json({ error: 'No password set' }, { status: 500 })
   }
